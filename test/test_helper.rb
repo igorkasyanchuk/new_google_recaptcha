e4b82d384b89033a84267e9e312330d72df79bb7
Marcin Kalita <rubyconvict@gmail.com>
2018-12-31 16:06:46 +0100

    Add webmock and byebug gems to ease testing & dev

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
+require "webmock/minitest"
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
@@ -19,3 +20,5 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
+
+WebMock.disable_net_connect!
