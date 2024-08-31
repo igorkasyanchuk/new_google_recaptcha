115af172e9ea4f6558bb9a23c63f18fb9216ae56
Serhii Khoma <srghma@gmail.com>
2024-08-31 13:46:18 +0700

    feat: expose actions_match

--- lib/new_google_recaptcha/validator.rb
+++ lib/new_google_recaptcha/validator.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 module NewGoogleRecaptcha
   class Validator
-    attr_reader :score, :result
+    attr_reader :score, :result, :actions_match
     attr_reader :token, :action, :minimum_score
 
     def initialize(token:, action:, minimum_score:)
@@ -12,19 +12,21 @@ module NewGoogleRecaptcha
     end
 
     def call
-      uri    = NewGoogleRecaptcha.compose_uri(token)
+      uri = NewGoogleRecaptcha.compose_uri(token)
 
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if uri.scheme == 'https'
       @result = JSON.parse(http.request(Net::HTTP::Get.new(uri)).body)
 
-      @score = result['score'].to_f
+      @score = @result['score'].to_f
 
-      conditions = []
-      conditions << !!result['success']
-      conditions << (result['score'].to_f >= minimum_score)
-      conditions << (result['action'].to_s == action.to_s)
-      conditions.none?(&:!)
+      @result__success = @result['success']
+      result__action = @result['action'].to_s
+
+      @score_bigger_than_minimum = @score >= minimum_score
+      @actions_match = result__action == action.to_s
+
+      @result__success && @score_bigger_than_minimum && @actions_match
     end
   end
 end
